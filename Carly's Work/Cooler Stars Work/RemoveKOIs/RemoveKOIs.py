b9c5dc0624c5c573c7007539334831aa59caa3c4
Chessler <chessler1337@gmail.com>
2014-10-15 10:25:50 -0400

    Reworking some things (One less star is missing)

--- Carly's Work/Cooler Stars Work/RemoveKOIs/RemoveKOIs.py
+++ Carly's Work/Cooler Stars Work/RemoveKOIs/RemoveKOIs.py
@@ -38,8 +38,8 @@ def writeToFile(fileName):
     for row in FullArr:
         writer.writerow(row)
 
-KOIArr = reader('C:/Users/Carly/Documents/fgcu-stellar-research/Carly\'s Work/Cooler Stars Work/KOITargets.csv')
-FullArr = reader('C:/Users/Carly/Documents/fgcu-stellar-research/Carly\'s Work/Cooler Stars Work/CoolerStarsTargetList.csv')
+FullArr = reader('C:/Users/cahessler3098/Documents/fgcu-stellar-research/Carly\'s Work/Cooler Stars Work/RemoveKOIs/InitialKOIList.csv')
+KOIArr = reader('C:/Users/cahessler3098/Documents/fgcu-stellar-research/Carly\'s Work/Cooler Stars Work/KOITargets.csv')
 
 temp = len(FullArr)
 print(len(FullArr))
